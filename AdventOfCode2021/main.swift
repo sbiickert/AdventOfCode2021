eb36c471db70ca2cb5d25aac108a8cad1d0348ae
Simon Biickert <simon@biickert.ca>
2021-11-24 09:25:20 -0700

    Added swift-algorithms in preparation.

--- AdventOfCode2021/main.swift
+++ AdventOfCode2021/main.swift
@@ -6,16 +6,21 @@
 //
 
 import Foundation
+import Algorithms
 
 print("Hello, Advent of Code!")
 
-// testInput(filename: "00.input")
+testInput(filename: "00.input")
 
 
 private func testInput(filename: String) {
 	let lines = AOCUtil.readInputFile(named: filename, removingEmptyLines: true)
 
 	print(lines)
+	for combo in lines.combinations(ofCount: 4) {
+		print(combo)
+	}
+	
 
 	let groups = AOCUtil.readGroupedInputFile(named: filename)
 
