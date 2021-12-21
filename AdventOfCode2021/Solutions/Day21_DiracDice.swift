ce7e241d752cf754ac8fe2b5aba7d96d629ca9fa
Simon Biickert <simon@biickert.ca>
2021-12-21 16:06:29 -0700

    Day 21 part 2 complete.

--- AdventOfCode2021/Solutions/Day21_DiracDice.swift
+++ AdventOfCode2021/Solutions/Day21_DiracDice.swift
@@ -7,38 +7,41 @@
 //
 
 import Foundation
+import Algorithms
 
 struct DiracDice {
+		static let BOARD_SIZE = 10
 		static func solve(startingPositions: (p1: Int, p2: Int)) {
 		print("\nDay 21 (Dirac Dice) -> \(startingPositions)")
 		
-		let practiceScore = solvePartOne(startingPositions: startingPositions)
+		let part1Score = solvePartOne(startingPositions)
 		
 		print("Part One")
-		print("The practice game result was: \(practiceScore)")
+		print("The part 1 game result was: \(part1Score)")
+			
+		print("Part Two")
+		let multiverseWins = solvePartTwo(startingPositions)
+		print("The most winning universes is: \(multiverseWins)")
 	}
 	
-	static func solvePartOne(startingPositions: (p1: Int, p2: Int)) -> Int {
+	static func solvePartOne(_ startingPositions: (p1: Int, p2: Int)) -> Int {
 		var players = [Player]()
-		players.append(Player(label: "p1"))
-		players.append(Player(label: "p2"))
+		players.append(Player(position: startingPositions.p1))
+		players.append(Player(position: startingPositions.p2))
 		
-		let board = GameBoard(size: 10)
-		board.addPlayer(at: startingPositions.p1)
-		board.addPlayer(at: startingPositions.p2)
 		let die = DeterministicDie(sides: 100)
 		
 		var gameOver = false
 		while true {
-			for (index, player) in players.enumerated() {
-				var roll = die.roll()
-				board.movePlayer(index, by: roll)
-				roll = die.roll()
-				board.movePlayer(index, by: roll)
-				roll = die.roll()
-				player.score +=	board.movePlayer(index, by: roll)
+			for index in 0..<players.count {
+				for _ in 1...3 {
+					let roll = die.roll()
+					let newPos = boardMove(from: players[index].position, by: roll)
+					players[index].position = newPos
+				}
+				players[index].score +=	players[index].position
 
-				if player.score >= 1000 {
+				if players[index].score >= 1000 {
 					gameOver = true
 					break
 				}
@@ -48,42 +51,99 @@ struct DiracDice {
 		players.sort(by: {$0.score > $1.score})
 		let winner = players.first!
 		let loser = players.last!
-		print("Player \(winner.label) won. Score: \(winner.score)")
+		//print("Player \(winner.label) won. Score: \(winner.score)")
 		return loser.score * die.rollCount
 	}
-}
 
-class Player {
-	let label: String
-	var score: Int = 0
-	
-	init(label: String) {
-		self.label = label
-	}
-}
-
-class GameBoard {
-	let size: Int
-	var playerPositions = [Int]()
-
-	init(size: Int) {
-		self.size = size
-	}
-	
-	func addPlayer(at pos: Int) {
-		playerPositions.append(pos)
-	}
-	
-	@discardableResult
-	func movePlayer(_ index: Int, by roll: Int) -> Int {
-		let curPos = playerPositions[index]
-		var newPos = curPos + roll
-		while newPos > size {
-			newPos -= size
+	static func solvePartTwo(_ startingPositions: (p1: Int, p2: Int)) -> Int {
+		// Every player's turn will have 27 possible roll combinations
+		// But only in a range of 3 to 9, with a distribution like:
+		// sum:  3  4  5  6  7  8  9
+		// dist: 1  3  6  7  6  3  1
+		let distr = [3:1, 4:3, 5:6, 6:7, 7:6, 8:3, 9:1]
+		
+		var uniqueGameStates = Dictionary<GameState, Int>()
+		uniqueGameStates[GameState(p1: Player(position: startingPositions.p1), p2: Player(position: startingPositions.p2))] = 1
+		
+		// Start looping
+		while uniqueGameStates.keys.filter({$0.gameOver == false}).count > 0 {
+			for playerIndex in 1...2 {
+				var nextTurnGameStates = Dictionary<GameState, Int>()
+				for (gameState,count) in uniqueGameStates {
+					if gameState.gameOver == false {
+						for (rollSum,rollSumCount) in distr {
+							var newP1 = gameState.p1
+							var newP2 = gameState.p2
+							if playerIndex == 1 {
+								let newPos = boardMove(from: gameState.p1.position, by: rollSum)
+								newP1.position = newPos
+								newP1.score = gameState.p1.score + newPos
+							}
+							else {
+								let newPos = boardMove(from: gameState.p2.position, by: rollSum)
+								newP2.position = newPos
+								newP2.score = gameState.p2.score + newPos
+							}
+							var newGS = GameState(p1: newP1, p2: newP2)
+							
+							let c = count * rollSumCount
+							if nextTurnGameStates.keys.contains(newGS) == false {
+								nextTurnGameStates[newGS] = 0
+							}
+							nextTurnGameStates[newGS]! += c
+						}
+					}
+					else {
+						if nextTurnGameStates.keys.contains(gameState) == false {
+							nextTurnGameStates[gameState] = 0
+						}
+						nextTurnGameStates[gameState]! += count
+					}
+				}
+				uniqueGameStates = nextTurnGameStates
+			}
 		}
-		playerPositions[index] = newPos
+		
+		var p1TotalWins = 0
+		var p2TotalWins = 0
+		for (gameState, count) in uniqueGameStates {
+			if gameState.p1.score > gameState.p2.score {
+				p1TotalWins += count
+			}
+			else {
+				p2TotalWins += count
+			}
+		}
+		
+		print("Player 1 wins: \(p1TotalWins)")
+		print("Player 2 wins: \(p2TotalWins)")
+
+		return max(p1TotalWins, p2TotalWins)
+	}
+	
+	static func boardMove(from oldPos: Int, by roll: Int) -> Int {
+		var newPos = oldPos + roll
+		while newPos > BOARD_SIZE {
+			newPos -= BOARD_SIZE
+		}
 		return newPos
 	}
+}
+
+struct GameState: Hashable {
+	static let WINNING_SCORE = 21
+	let p1: Player
+	let p2: Player
+	
+	var gameOver: Bool {
+		return p1.score >= GameState.WINNING_SCORE || p2.score >= GameState.WINNING_SCORE
+	}
+}
+
+struct Player: Hashable {
+	//let label: String
+	var position: Int
+	var score: Int = 0
 }
 
 protocol Die {
@@ -110,6 +170,4 @@ class DeterministicDie: Die {
 		rollCount += 1
 		return result
 	}
-	
-	
 }
