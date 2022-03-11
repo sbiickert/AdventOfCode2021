02698101bc787c08ebaacadb75744d1bc0caed83
Simon Biickert <simon@biickert.ca>
2022-03-10 17:55:50 -0700

    Continuing Day 23

--- Perl/day23.pl
+++ Perl/day23.pl
@@ -31,6 +31,7 @@ use Storable 'dclone';
 
 my %COST = (A => 1, B => 10, C => 100, D => 1000);
 my %LINK;
+my %PATH;
 my $WINNING_STATE;
 my $ROOM_SIZE;
 my %STATES;
@@ -74,13 +75,73 @@ sub make_moves {
 sub find_legal_moves {
 	my %current = %{ $_[0] };
 	
+	my @locs = find_occupant_locations(\%current);
+	say "Occupants are at " . join(' ', @locs);
+	for my $loc (@locs) {
+		my $room = substr($loc, 0, 1);
+		my $index = substr($loc, 1, 1);
+		my $mover = $current{$room}[$index];
+		say "$room $index -> $mover";
+		
+		my @legal_destinations;
+		if ($room eq 'N') {
+			# hallway
+			if (! grep { $_ ne '' and $_ ne $mover } @{ $current{$room} }) {
+				# there are no 'foreigners' in the destination room
+				for (my $i = $ROOM_SIZE-1; $i >= 0; $i--) {
+					if ($current{$room}[$index] eq '') {
+						# this is the deepest empty slot in the room. Can navigate to it?
+						my @path = find_path(\%current, $loc, "$room$index");
+						
+						
+						
+						
+						
+						 
+					}
+				}
+			}
+		}
+		else {
+			# room
+		
+		}
+	}
+}
+
+sub find_occupant_locations {
+	my %burrow = %{ $_[0] };
+	
+	my @locations;
+	
+	for my $r ('N', 'A'..'D') {
+		my @room = @{$burrow{$r}};
+		for my $i (0..$#room) {
+			push(@locations, "$r$i") if $room[$i] ne '';
+		}
+	}
+	return @locations;
+}
+
+sub find_path {
+	my ($b_ref, $from, $to) = @_;
+	my %burrow = %$b_ref;
+ @{ $PATH{} };	
+}
+
+sub get_at_loc {
+	my ($b_ref, $loc) = @_;
+	my %burrow = %$b_ref;
+	my $room = substr($loc, 0, 1);
+	my $index = substr($loc, 1, 1);
+	return $burrow{$room}[$index];
 }
 
 sub init {
 	my ($type, $part) = @_;
 
 	my %burrow = (
-		HALL => ['','','','','','',''],
+		N => ['','','','','','',''],
 		A => ['',''],
 		B => ['',''],
 		C => ['',''],
@@ -122,6 +183,7 @@ sub init {
 		push(@{$winning_burrow{'C'}}, 'C', 'C');
 		push(@{$winning_burrow{'D'}}, 'D', 'D');
 	}
+	build_paths();
 	
 	$WINNING_STATE = encode_burrow(\%winning_burrow);
 	
@@ -148,12 +210,79 @@ sub build_links {
 	}
 }
 
+sub build_paths {
+	%PATH = (
+		'A0-N0' => ['A0', 'N1', 'N0'],
+		'A0-N1' => ['A0', 'N1'],
+		'A0-N2' => ['A0', 'N2'], 
+		'A0-N3' => ['A0', 'N2', 'N3'], 
+		'A0-N4' => ['A0', 'N2', 'N3', 'N4'], 
+		'A0-N5' => ['A0', 'N2', 'N3', 'N4', 'N5'], 
+		'A0-N6' => ['A0', 'N2', 'N3', 'N4', 'N5', 'N6'],
+		
+		'B0-N0' => ['B0', 'N2', 'N1', 'N0'],
+		'B0-N1' => ['B0', 'N2', 'N1'],
+		'B0-N2' => ['B0', 'N2'], 
+		'B0-N3' => ['B0', 'N3'], 
+		'B0-N4' => ['B0', 'N3', 'N4'], 
+		'B0-N5' => ['B0', 'N3', 'N4', 'N5'], 
+		'B0-N6' => ['B0', 'N3', 'N4', 'N5', 'N6'],
+		
+		'C0-N0' => ['C0', 'N3', 'N2', 'N1', 'N0'],
+		'C0-N1' => ['C0', 'N3', 'N2', 'N1'],
+		'C0-N2' => ['C0', 'N3', 'N2'], 
+		'C0-N3' => ['C0', 'N3'], 
+		'C0-N4' => ['C0', 'N4'], 
+		'C0-N5' => ['C0', 'N4', 'N5'], 
+		'C0-N6' => ['C0', 'N4', 'N5', 'N6'],
+		
+		'D0-N0' => ['D0', 'N4', 'N3', 'N2', 'N1', 'N0'],
+		'D0-N1' => ['D0', 'N4', 'N3', 'N2', 'N1'],
+		'D0-N2' => ['D0', 'N4', 'N3', 'N2'], 
+		'D0-N3' => ['D0', 'N4', 'N3'], 
+		'D0-N4' => ['D0', 'N4'], 
+		'D0-N5' => ['D0', 'N5'], 
+		'D0-N6' => ['D0', 'N5', 'N6']
+	);
+	
+	# Build the paths from the deeper points in the rooms
+	for my $letter ('A'..'D') {
+		my $key_prefix = $letter . '0';
+		for my $key (keys %PATH) {
+			if ($key =~ m/^${key_prefix}(.+)/) {
+				for my $i (0..$ROOM_SIZE-2) {
+					my $j = $i+1;
+					my $new_key = "$letter$j$1";
+					my @temp = $PATH{$key};
+					#print Dumper(\@temp);
+					my $ref = dclone( \@temp );
+					my @path = @$ref;
+					unshift(@{$path[0]}, "$letter$j");
+					#print Dumper(\@path);
+
+					$PATH{$new_key} = \@{$path[0]};
+				}
+			}
+		}
+	}
+
+	# Reverse all paths
+	for my $key (keys %PATH) {
+		if ($key =~ m/(\w\d)-(\w\d)/) {
+			my $rev_key = "$2-$1";
+			my @temp = @{$PATH{$key}};
+			my @rev_path = reverse @temp;
+			$PATH{$rev_key} = \@rev_path;
+		}
+	}
+}
+
 sub print_burrow {
 	my ($b_ref, $cost) = @_;
 	my %burrow = %$b_ref;
 	say "Cost: " . sprintf('%7d', $cost);
 	say '#' x 13;
-	my @hall = map { $_ eq '' ? '.' : $_ } @{$burrow{'HALL'}};
+	my @hall = map { $_ eq '' ? '.' : $_ } @{$burrow{'N'}};
 	say '#' . $hall[0] . join('.', @hall[1..5]) . $hall[6] . '#';
 	for my $i (0..$ROOM_SIZE-1) {
 		say '###' . join('#', $burrow{'A'}[$i], $burrow{'B'}[$i],
@@ -164,8 +293,8 @@ sub print_burrow {
 
 sub encode_burrow {
 	my %burrow = %{ $_[0] };
-	my @mapped = map { $_ eq '' ? '.' : $_ } @{$burrow{'HALL'}};
-	my $enc = 'HALL:' . join('-', @mapped) . ',';
+	my @mapped = map { $_ eq '' ? '.' : $_ } @{$burrow{'N'}};
+	my $enc = 'N:' . join('-', @mapped) . ',';
 	for my $i ('A'..'D') {
 		@mapped = map { $_ eq '' ? '.' : $_ } @{$burrow{$i}};
 		$enc .= "$i:" . join('-', @mapped) . ',';
