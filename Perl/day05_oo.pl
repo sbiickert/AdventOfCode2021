193e684a5e2895a08bf4e7465319bb351e846db4
Simon Biickert <simon@biickert.ca>
2022-03-20 08:41:59 -0600

    Experiment with OO Perl and having an AOC Module

---
+++ Perl/day05_oo.pl
@@ -0,0 +1,137 @@
+#!/usr/bin/env perl
+BEGIN {
+    use Cwd;
+    our $directory = cwd;
+}
+
+use lib $directory;
+
+use Modern::Perl;
+use autodie;
+use Data::Dumper;
+use List::Util 'max';
+use Storable 'dclone';
+
+use AOC::Geometry qw(Point2D Line2D);
+
+my $INPUT_PATH = '../AdventOfCode2021/Input Files';
+
+my @test_input = parse_input("$INPUT_PATH/05.test.txt");
+my @real_input = parse_input("$INPUT_PATH/05.challenge.txt");
+
+#solve_part_one(@test_input);
+solve_part_one(@real_input);
+
+#solve_part_two(@test_input);
+solve_part_two(@real_input);
+
+
+exit( 0 );
+
+sub parse_input {
+	my $input_file = shift;
+	
+	open my $input, '<', $input_file or die "Failed to open input: $!";
+	
+	my @lines = ();
+	
+	while (<$input>) {
+		$_ =~ m/^(\d+),(\d+)\D+(\d+),(\d+)$/;
+		my %vent_line;
+		my $start = Point2D->new('px' => $1+0, 'py' => $2+0); #numify
+		my $end = 	Point2D->new('px' => $3+0, 'py' => $4+0); #numify
+		$vent_line{'line'} = Line2D->new('from' => $start, 'to' => $end);
+		$vent_line{'horizontal'} = $start->py == $end->py; # no change in y
+		$vent_line{'vertical'} = $start->px == $end->px;   # no change in x
+		$vent_line{'points'} = interpolate($vent_line{'line'});
+		push(@lines, \%vent_line);
+	}
+	
+	close $input;
+	
+	return @lines;
+}
+
+sub interpolate {
+	my ($line) = shift;
+	my @points = ();
+	my $dx = $line->dx;
+	my $dy = $line->dy;
+	my $length = max(abs($dx), abs($dy));
+	my $stepx = $dx != 0 ? $dx / abs($dx) : 0;
+	my $stepy = $dy != 0 ? $dy / abs($dy) : 0;
+	
+	#aliasing so we don't store the reference.
+	my $start = dclone($line->from);
+	#print Dumper($line, $start);
+	push(@points, $start);
+	
+	my $prev = $start;
+	for my $i (1..$length) {
+		my $pt = Point2D->new('px' => $prev->px + $stepx, 'py' => $prev->py + $stepy);
+		push(@points, $pt);
+		$prev = $pt;
+	}
+	
+	return \@points;
+}
+
+sub solve_part_one {
+	my @lines = @_;
+	my @grid = ();
+	
+	for my $line_ref (@lines) {
+		if ($line_ref->{'horizontal'} or $line_ref->{'vertical'}) {
+			for my $pt (@{$line_ref->{'points'}}) {
+				$grid[$pt->py][$pt->px] += 1;
+			}
+		}
+	}
+	#print_grid(@grid);
+	my $count = count_overlaps(@grid);
+	
+	print "Part One\n";
+	print "The overlapping point count is $count\n";
+}
+
+sub solve_part_two {
+	my @lines = @_;
+	my @grid = ();
+	
+	for my $line_ref (@lines) {
+		for my $pt (@{$line_ref->{'points'}}) {
+			$grid[$pt->py][$pt->px] += 1;
+		}
+	}
+	#print_grid(@grid);
+	my $count = count_overlaps(@grid);
+	
+	print "Part Two\n";
+	print "The overlapping point count is $count\n";
+}
+
+sub print_grid {
+	my @grid = @_;
+	my $size = scalar(@grid);
+	
+	for my $row_ref (@grid) {
+		for my $i (0..$size-1) {
+			print defined($row_ref->[$i]) ? $row_ref->[$i] : '.';
+		}
+		print "\n";
+	}
+}
+
+sub count_overlaps {
+	my @grid = @_;
+	my $size = scalar(@grid);
+	my $count = 0;
+	
+	for my $row_ref (@grid) {
+		for my $i (0..$size-1) {
+			$count++ if (defined($row_ref->[$i]) && $row_ref->[$i] > 1);
+		}
+	}
+	
+	return $count;
+}
